8bcc3448860c88698e808409fda6b251366a79fd
Sullfred <daniel.v.kleist@gmail.com>
2025-07-22 12:30:48 +0200

    Create app

---
+++ Cocktails 4.0/Cocktails 4.0Tests/Cocktails_4_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktails_4_0Tests.swift
+//  Cocktails 4.0Tests
+//
+//  Created by Daniel Vang Kleist on 22/07/2025.
+//
+
+import Testing
+@testable import Cocktails_4_0
+
+struct Cocktails_4_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
